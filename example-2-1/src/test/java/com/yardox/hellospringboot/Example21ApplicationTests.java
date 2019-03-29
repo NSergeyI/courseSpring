9d9bf6b9f69cdaea162217930a7ff36e327e31c7
SNikitinI <snikitin@akolchin.com>
2019-03-29 17:55:04 +0300

    example2 start

---
+++ example-2-1/src/test/java/com/yardox/hellospringboot/Example21ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yardox.hellospringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Example21ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
